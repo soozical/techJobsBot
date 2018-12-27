8a7527624b17c8447792e27ff9d7d70e1c197da5
Susie <susie.odneal@gmail.com>
2018-12-27 14:03:05 -0600

    added to lists of words

---
+++ bot.py
@@ -0,0 +1,9 @@
+import tweepy
+import sys
+import random
+from secrets import *
+
+auth = tweepy.OAuthHandler(C_KEY, C_SECRET)
+auth.set_access_token(A_TOKEN, A_TOKEN_SECRET)
+api = tweepy.API(auth)
+
